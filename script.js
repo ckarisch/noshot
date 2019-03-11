aec6fcfd709211ee2a8d105fd4dccd5e1766fab1
Christof Karisch <ckarisch@outlook.com>
2019-03-11 21:49:02 +0100

    added workspaces; removed marking

--- script.js
+++ script.js
@@ -4,35 +4,42 @@
 // localStorage persistence
 var STORAGE_KEY = 'DIVE-layout'
 var searchStorage = {
-  fetch: function () {
-    var searches = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+  fetchSearches: function () {
+    var searches = JSON.parse(localStorage.getItem(STORAGE_KEY+"_searches") || '[]')
     searches.forEach(function (search, index) {
       search.id = index
+      // for(var w in app.data.workspaces) {
+      //   if(search.workspace.id == w.id) {
+      //     search.workspace = w;
+      //   }
+      // }
     })
     searchStorage.uid = searches.length
     return searches
   },
+  fetchWorkspaces: function () {
+    var w = JSON.parse(localStorage.getItem(STORAGE_KEY+"_workspaces") || '[]');
+    if (!w || !w.length) {
+      w = [{id: 'default', name: 'Workspace 0'}];
+    }
+    return w;
+  },
   save: function (searches) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(searches))
+    localStorage.setItem(STORAGE_KEY+"_searches", JSON.stringify(searches))
+  },
+  saveWorkspaces: function (workspaces) {
+    localStorage.setItem(STORAGE_KEY+"_workspaces", JSON.stringify(workspaces))
   }
 }
 
 var fotos = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 
+
+
 // visibility filters
 var filters = {
   all: function (searches) {
     return searches
-  },
-  normal: function (searches) {
-    return searches.filter(function (search) {
-      return !search.marked
-    })
-  },
-  marked: function (searches) {
-    return searches.filter(function (search) {
-      return search.marked
-    })
   }
 }
 
@@ -40,10 +47,14 @@ var filters = {
 var app = new Vue({
   // app initial state
   data: {
-    searches: searchStorage.fetch(),
+    workspaces: searchStorage.fetchWorkspaces(),
+    searches: searchStorage.fetchSearches(),
     newSearch: '',
+    newTab: '',
+    tabCheck: true,
     editedSearch: null,
-    visibility: 'all'
+    visibility: 'default',
+    activeWorkspace: undefined
   },
 
   // watch searches change for localStorage persistence
@@ -53,17 +64,43 @@ var app = new Vue({
         searchStorage.save(searches)
       },
       deep: true
-    }
-  },
+    },
+    workspaces: {
+      handler: function (workspaces) {
+        searchStorage.saveWorkspaces(workspaces)
+      },
+      deep: true
+    },
+    newTab: {
+      handler: function (newTab) {
+        this.tabCheck = true;
+        for (var w of this.workspaces)
+        {
+          if ( w.id.toLowerCase() == this.newTab.toLowerCase())
+          {
+            this.tabCheck = false;
+          }
+        }
+      },
+      deep: true
+
+    }  },
 
   // computed properties
   // http://vuejs.org/guide/computed.html
   computed: {
     filteredSearches: function () {
-      return filters[this.visibility](this.searches)
+      //return filters[this.visibility](this.searches)
+      return this.searches.filter(function (search) {
+        return search.workspace == app.visibility || search.workspace == 0 || search.workspace === undefined
+        return true
+      })
     },
     fotos: function() {
       return fotos
+    },
+    getWorkspaces: function() {
+      return this.workspaces
     },
     all: function () {
       return this.searches.length
@@ -71,18 +108,8 @@ var app = new Vue({
     remaining: function () {
       return filters.normal(this.searches).length
     },
-    marked: function () {
-      return filters.marked(this.searches).length
-    },
-    allDone: {
-      get: function () {
-        return this.remaining === 0
-      },
-      set: function (value) {
-        this.searches.forEach(function (search) {
-          search.marked = value
-        })
-      }
+    getTabCheck: function () {
+      return this.tabCheck
     }
   },
 
@@ -100,17 +127,69 @@ var app = new Vue({
       if (!value) {
         return
       }
-      this.searches.push({
+      var s = {
         id: searchStorage.uid++,
         title: value,
-        marked: false,
-        frames: true
-      })
+        frames: true,
+        workspace: this.visibility
+      }
+      this.searches.push(s)
+      if(!this.activeWorkspace.searches)
+        this.activeWorkspace.searches = [];
+      this.activeWorkspace.searches.push(s)
       this.newSearch = ''
     },
+    addTab: function() {
+      var value = this.newTab && this.newTab.trim()
+      if (!value) {
+        return
+      }
+      if (!this.tabCheck)
+      {
+        return;
+      }
+      this.workspaces.push({ id: this.newTab, name: this.newTab });
+
+      this.visibility = this.newTab;
+      window.location.hash = this.visibility;
+      this.newTab = '';
+    },
+    // checkTabName: function () {
+    //   this.tabCheck = true;
+    //   for (var w in this.workspaces)
+    //   {
+    //     console.log( w.id.toLowerCase() + " , " + this.newTab.toLowerCase())
+    //     if ( w.id.toLowerCase() == this.newTab.toLowerCase())
+    //     {
+    //       this.tabCheck = false;
+    //     }
+    //   }
+    // },
 
     removeSearch: function (search) {
       this.searches.splice(this.searches.indexOf(search), 1)
+    },
+
+    removeWorkspace: function (workspace) {
+
+      // for(var s in workspace.searches) {
+      //   this.searches.splice(this.searches.indexOf(s), 1)
+      // }
+      var temp = [];
+      for(let search of this.searches) {
+        temp.push(search);
+      }
+
+      for(var i = 0; i < this.searches.length; i++)
+      {
+        if(this.searches[i].workspace == workspace.id) {
+          this.searches.splice(this.searches.indexOf(this.searches[i]), 1)
+          i--;
+        }
+      }
+
+      this.workspaces.splice(this.workspaces.indexOf(workspace), 1);
+      this.visibility = 'default';
     },
 
     editSearch: function (search) {
@@ -132,10 +211,6 @@ var app = new Vue({
     cancelEdit: function (search) {
       this.editedSearch = null
       search.title = this.beforeEditCache
-    },
-
-    removeMarked: function () {
-      this.searches = filters.normal(this.searches)
     }
   },
 
@@ -154,12 +229,26 @@ var app = new Vue({
 // handle routing
 function onHashChange () {
   var visibility = window.location.hash.replace(/#\/?/, '')
-  if (filters[visibility]) {
-    app.visibility = visibility
-  } else {
-    window.location.hash = ''
-    app.visibility = 'all'
+  app.visibility = 'default'
+
+  for(var w of app.workspaces)
+  {
+    if(w.id == visibility)
+    {
+      app.visibility = visibility;
+      app.activeWorkspace = w;
+    }
   }
+  if( app.visibility == 'default') {
+    window.location.hash = ''
+  }
+
+  // if (filters[visibility] || app.workspaces[visibility]) {
+  //   app.visibility = visibility
+  // } else {
+  //   window.location.hash = ''
+  //   app.visibility = 'all'
+  // }
 }
 
 window.addEventListener('hashchange', onHashChange)
