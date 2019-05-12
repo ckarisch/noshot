7d1bedbd80ce944968addbcba9689beb9a26ca88
Christof Karisch <ckarisch@outlook.com>
2019-05-12 17:52:34 +0200

    add server

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+
+  http.get({
+    hostname: 'localhost',
+    port: 8983,
+    path: '/solr/core1/select?q=*%3A*&fl=430',
+    agent: false // Create a new agent just for this one request
+  }, (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      let docs = JSON.parse(data).response.docs;
+
+      //console.log(data);
+      res.end("Data:" + JSON.stringify(docs));
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+    res.end("Error: " + err.message);
+  });
+
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
