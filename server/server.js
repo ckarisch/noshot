432e474383b22668502a342428f94b9bb2da3960
Christoph <ckarisch@outlook.com>
2019-05-20 19:51:24 +0200

    bugfix

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 var express = require('express');
 var app = express();
+var util = require('util');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -16,11 +17,12 @@ app.get('/category/:category?', function userIdHandler(req, res) {
   res.setHeader('Content-Type', 'application/json');
 
   let category = req.params.category;
+  let net = "cnn_googlenet";
 
   http.get({
     hostname: 'localhost',
     port: 8983,
-    path: '/solr/core1/select?q=category%3A' + category + '&rows=100',
+    path: util.format('/solr/core1/select?q=net%3A%s%20AND%20category%3A%s&rows=%i&sort=probability%20desc', net, category, 100),
     agent: false // Create a new agent just for this one request
   }, (resp) => {
     let data = '';
@@ -32,6 +34,7 @@ app.get('/category/:category?', function userIdHandler(req, res) {
 
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
+      console.log(data);
       let docs = JSON.parse(data).response.docs;
       res.json(docs);
     });
