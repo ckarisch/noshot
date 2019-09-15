91f5b1debfbe95e91febe9e9da364e9e9ec91941
Christof Karisch <ckarisch@outlook.com>
2019-09-15 21:22:45 +0200

    add categories

--- scripts/csv2json.py
+++ scripts/csv2json.py
@@ -9,12 +9,12 @@
 import sys, os, json
 
 # only store best x concepts of each net for each keyframe
-numBestProbabilities = 10
+numBestProbabilities = 5
 
 def floatformat(string):
 	return '{0:.7f}'.format(float(string))
 
-def line2dict(line):
+def line2tupel(line):
 	temp = []
 	parts = line.split(",")
 	for category, probability in zip( parts[0::2], map(floatformat, parts[1::2]) ):
@@ -22,26 +22,30 @@ def line2dict(line):
 
         # sort by second value (probability) with sorted
         # only return best x concepts
-	return dict(sorted(temp, key=lambda tup: tup[1])[:numBestProbabilities])
+	return sorted(temp, key=lambda tup: tup[1])[:numBestProbabilities]
 
 def file2json(filename):
 	with open(filename) as f:
 		line = f.readline().rstrip("\n")
-		return json.dumps(line2dict(line))
+		return line2tupel(line)
 
 def printFile(root, filename):
 	fullpath = os.path.join(root, filename)
 	afilename = filename.split("_")
 	netName = "_".join(afilename[2:]).split(".")[0]
-	jsonStr = file2json(fullpath)
+	classes = file2json(fullpath)
 
-	sys.stdout.write('"add": { "doc": {')
-	sys.stdout.write('"nodeType": "keyframe", ')
-	#sys.stdout.write('"video": {0}, "keyframe": {1}, "net": "{2}"'.format(int(afilename[0]), int(afilename[1]), netName))
-	#sys.stdout.write(', "{0}":{1}'.format(netName, jsonStr))
-	sys.stdout.write('"video": {0}, "keyframe": {1}, "net": "{2}"'.format(int(afilename[0]), int(afilename[1]), netName))
-	sys.stdout.write(', {0}'.format(jsonStr[1:-1]))
-	sys.stdout.write('}}')
+	for entry in classes:
+
+
+                sys.stdout.write('"add": { "doc": {')
+                sys.stdout.write('"nodeType": "keyframe", ')
+                #sys.stdout.write('"video": {0}, "keyframe": {1}, "net": "{2}"'.format(int(afilename[0]), int(afilename[1]), netName))
+                #sys.stdout.write(', "{0}":{1}'.format(netName, jsonStr))
+                sys.stdout.write('"video": {0}, "keyframe": {1}, "net": "{2}"'.format(int(afilename[0]), int(afilename[1]), netName))
+                #sys.stdout.write(', {0}'.format(jsonStr[1:-1]))
+                sys.stdout.write(', "category": {0}, "probability": {1}'.format(entry[0], entry[1]))
+                sys.stdout.write('}}')
 
 def walkRootFilename(directory):
 	walk = os.walk(directory)
@@ -65,10 +69,9 @@ for root, filename in walk:
 	sys.stdout.write(",")
 	printFile(root, filename)
 
-	if(counter >= end):
-		break
+	#if(counter >= end):
+	#	break
 	counter += 1
 	sys.stderr.write("\r{0}".format(counter))
 
 sys.stdout.write(', "commit": {} }')
-
