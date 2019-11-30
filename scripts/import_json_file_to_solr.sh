5d20825cba5dc477ff4737b41ebea977bf24cfdd
amplejoe <amplejoe@gmail.com>
2019-11-30 23:10:41 +0100

    made script callable with parameter

--- scripts/import_json_file_to_solr.sh
+++ scripts/import_json_file_to_solr.sh
@@ -1,1 +1,12 @@
-curl -X POST -H 'Content-Type: application/json' 'http://localhost:8983/solr/core1/update' --data-binary @data.json
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "usage: ./import_json_file_to_solr.sh [IMPORT_FILE]"
+    echo "encapsule paths within '' if they contain spaces"
+    exit 1
+fi
+
+args=("$@")
+IN_FILE="${args[0]}"
+
+curl -X POST -H 'Content-Type: application/json' 'http://localhost:8983/solr/core1/update' --data-binary @"${IN_FILE}"
