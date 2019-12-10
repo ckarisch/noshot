9287758f4b4ca767e18c0cc262c879bf7f537eef
aleibets <amplejoe@gmail.com>
2019-12-10 22:00:02 +0100

    simple cfg

--- noshot/src/main.js
+++ noshot/src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import config from '../config/default.json'
+import configLocal from '../config/local.json'
+
+// app config
+const localCfg = configLocal;
+window.appCfg = config
+if (Object.keys(localCfg).length > 0) {
+  // TODO parse local cfg
+}
 
 Vue.config.productionTip = false
 
