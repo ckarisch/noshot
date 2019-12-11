9bf3c419c69dc734c2fbf97c4efe82c8619743f0
anleibetsed <you@example.com>
2019-12-11 03:37:37 +0100

    update: simple ccfg & fix for missing local cfg

--- noshot/src/main.js
+++ noshot/src/main.js
@@ -1,13 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
-import config from '../config/default.json'
-import configLocal from '../config/local.json'
+import config from '../public/config/default.json' // default app cfg
 
-// app config
-const localCfg = configLocal;
+// global cfg
 window.appCfg = config
-if (Object.keys(localCfg).length > 0) {
-  // TODO parse local cfg
+
+// local cfg
+// try getting local cfg ('public' is served under serverurl:port/)
+fetch('./config/local.json', {
+    method: "GET"
+  })
+  .then(r => r.json()).then(json => {
+    // override keys set in local cfg
+    updateJSONRecursive(window.appCfg, json);
+  }, json => {
+    // not found / unexpected error
+    let msg = `Local config: ${json}`
+    // eslint-disable-next-line no-console
+    console.log(msg)
+  })
+// updates parts of default cfg according to local cfg
+function updateJSONRecursive(jsonPart, updatePart) {
+  if (Object.keys(jsonPart).length === 0) return;
+  if (Object.keys(updatePart).length === 0) return;
+  for (let key in updatePart) {
+    if (typeof updatePart[key] === 'object' &&
+      Object.keys(updatePart[key]).length > 0 &&
+      Object.keys(jsonPart[key].length > 0)) {
+      updateJSONRecursive(jsonPart[key], updatePart[key])
+    } else if (jsonPart[key] !== updatePart[key]) {
+      // eslint-disable-next-line no-console
+      console.log(`[config override] ${key}: ${jsonPart[key]} => ${updatePart[key]}`);
+      jsonPart[key] = updatePart[key];
+    }
+  }
 }
 
 Vue.config.productionTip = false
