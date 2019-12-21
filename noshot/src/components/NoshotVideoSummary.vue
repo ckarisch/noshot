20a73652130f4029f2ca9905d8e1cab42cb720f6
Andi <aleibets@itec.aau.at>
2019-12-21 03:12:06 +0100

    moved video summary loading to video summary class

--- noshot/src/components/NoshotVideoSummary.vue
+++ noshot/src/components/NoshotVideoSummary.vue
@@ -23,6 +23,9 @@ import NoshotImage from './NoshotImage.vue'
 export default {
     name: 'NoshotVideoSummary',
     created() {
+        if (this.search.images.length === 0) {
+            this.search.images = this.getVideoSummaryImages(this.search.video);
+        }
     },
     components: {
       NoshotImage
@@ -34,12 +37,24 @@ export default {
     data: () => {
       return {
         // errorLoading: (e) => {this.errorLoading(e)}
-
       }
     },
 
     methods: {
-
+        getVideoSummaryImages: function(video) {
+          let keyframeBase = this.appCfg.dataServer.url + ':' + this.appCfg.dataServer.port + '/' + this.appCfg.dataServer.keyframesLocation + '/';
+          // let testimg = document.querySelector('.testimg');
+          let totalKeyFrames = parseInt(this.appCfg.keyCount[video.id]);
+          let s = [];
+          for (let i = 0; i < totalKeyFrames; i++) {
+            let src = `${keyframeBase}${video.id}/${video.id}_${i}_key.jpg`;
+            let v = this.utils.videoFromThumbUrl(src);
+            v.second = i;
+            let fakeDBResult = this.utils.videoToDBResult(v);
+            s.push(fakeDBResult);
+          }
+          return s;
+        }
     },
     mounted: function() {
 
